92884d118d59f93f46a601c1f981454be34c6e0c
unknown <cslag@hotmail.com.tw>
2015-01-30 19:21:53 +0800

    Master pull

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var fs = require('fs');
 var md = require('./lib');
 
+console.time('timer');
 var ms = md('./Database','Test','Users');
 var d2 = md('./Database','Test2','Users');
 
-ms.insert({'name':'tessst'});
+/*ms.insert({'name':'tessst'});
 d2.insert([{"test":'testtoo'}]);
 ms.insert({'name':'tessst'});
-d2.insert([{"test":'testtoo'}]);
+d2.insert([{"test":'testtoo'}]);*/
+
+ms.find([],function(row){
+	console.log(row);
+});
+console.timeEnd('timer');
