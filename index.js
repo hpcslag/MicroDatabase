dcaee3523f5b5c9610b4779e732e7b51eb5cdf34
unknown <cslag@hotmail.com.tw>
2015-02-03 00:44:28 +0800

    auto routing

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 var fs = require('fs');
 var md = require('./lib');
 
+function DatabaseControl(db_name,db_colle){
+	//use db_name and colle find db
+	var db_path = './Database';
+	return require('./lib')(db_path,db_name,db_colle);
+}
 
-var ms = md('./Database','Test','Users');
-var d2 = md('./Database','Test2','Users');
-
-/*ms.insert({'name':'tessst'});
-d2.insert([{"test":'testtoo'}]);
-ms.insert({'name':'tessst'});
-d2.insert([{"test":'testtoo'}]);*/
-console.time('timer');
-/*ms.find([{age:16}],function(row){
-	console.log(row);
-});*/
-/*ms.findOne({age:16},function(match){
-	console.log(match);
-});*/
-//ms.update({age:16},{age:18});
-//ms.remove({age:16});
-console.timeEnd('timer');
+module.exports = DatabaseControl;
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-throw new Error('not thing');
+var aa = require('./');
+
+var gg = aa('Test2','Users');
+
+gg.find('any string or tag',function(row){
+	console.log(row);
+});
