277e21e25cd7d3e50a5817ebb9c264840074757d
unknown <cslag@hotmail.com.tw>
2015-02-03 14:49:29 +0800

    Added Language Feature

--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-var aa = require('./');
-
 var test = require('./lib/DatabaseInfo.js');
-
-var gg = aa('Test2','Users');
-
 //console.log(test.getDataStruct)
 
 //test.setPath('C:\\Users\\MacTaylors\\Desktop\\Microdatabase\\Database');
 
-console.log(test.findDB('DBName','CollectionName1'));
+//console.log(test.findDB('DBName','CollectionName1'));
 
+
+var gg = aa('Test2','Users');
+var aa = require('./');
 /*gg.find('any string or tag',function(row){
 	console.log(row);
 });*/
