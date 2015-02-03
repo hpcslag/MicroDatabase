b1b2d2cf591210d591605e1ef485de56fb442b09
unknown <cslag@hotmail.com.tw>
2015-02-03 15:16:56 +0800

    added moveINDEX function in setPath function

--- test.js
+++ test.js
@@ -6,8 +6,8 @@ var test = require('./lib/DatabaseInfo.js');
 //console.log(test.findDB('DBName','CollectionName1'));
 
 
-var gg = aa('Test2','Users');
-var aa = require('./');
+/*var gg = aa('Test2','Users');
+var aa = require('./');*/
 /*gg.find('any string or tag',function(row){
 	console.log(row);
 });*/
