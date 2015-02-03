8d1a2c027c2a4ef1ba46af5191064037157f71cd
unknown <cslag@hotmail.com.tw>
2015-02-03 16:38:29 +0800

    added find collections and find DB fix

--- test.js
+++ test.js
@@ -2,9 +2,12 @@ var test = require('./lib/DatabaseInfo.js');
 //console.log(test.getDataStruct)
 
 //test.setPath('C:\\Users\\MacTaylors\\Desktop\\Microdatabase\\Database');
-test.removeColle("Test",'Users');
+//test.removeColle("Test",'Users');
 //console.log(test.findDB('DBName','CollectionName1'));
 
+//console.log(test.findDB());
+
+//console.log(test.findCollections("DBNamex"));
 
 /*var gg = aa('Test2','Users');
 var aa = require('./');*/
